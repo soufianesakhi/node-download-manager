2ca4ccc53406a5b51ff03a7f9b6929f96b149fd6
Soufiane Sakhi <soufiane.sakhi.js@gmail.com>
2018-02-22 19:21:06 +0100

    [ng] Add copy buttons

--- src/app/downloads-list/downloads-list.component.ts
+++ src/app/downloads-list/downloads-list.component.ts
@@ -31,4 +31,29 @@ export class DownloadsListComponent implements OnInit {
     this.selectedLinksMarginTop = selectedOffsetTop - containerOffsetTop;
     this.selectedLinks = selectedLinks;
   }
+
+  getAllLinks(selectedLinks: DownloadLinksModel) {
+    const allLinks: string[] = [];
+    selectedLinks.links.forEach(l => allLinks.push(...l));
+    return allLinks.join("\r\n");
+  }
+
+  copyFullTitle() {
+    this.copyText(this.selectedLinks.artist + " - " + this.selectedLinks.title);
+  }
+
+  copyAllLinks() {
+    this.copyText(this.getAllLinks(this.selectedLinks));
+  }
+
+  private copyText(text: string) {
+    const input = document.createElement("textarea");
+    input.textContent = text;
+    document.body.appendChild(input);
+    input.select();
+    const successful = document.execCommand('copy');
+    input.remove();
+    alert(successful ? 'Copied' : 'Not copied');
+  }
+
 }
