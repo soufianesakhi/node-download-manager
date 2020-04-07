c6c1c8fb4d8db020c28d5bfc86020b89ac3b4ae3
Soufiane Sakhi <soufiane.sakhi.js@gmail.com>
2020-04-07 23:59:38 +0200

    Changed node test port, spi

--- src/server/spi.ts
+++ src/server/spi.ts
@@ -1,43 +1,35 @@
-import { Express, Router } from 'express';
-import * as fs from 'fs';
-import * as path from 'path';
-import { DownloadSPI } from '..';
+import { Express, Router } from "express";
+import * as fs from "fs";
+import * as path from "path";
 
-export const SPIPath = '/spi';
-const metadata: DownloadSPI = {
-    supported: false,
-    path: "",
-    ignoredCats: []
-};
+export const SPIPath = "/spi";
 
 export function registerSPI(app: Express) {
-    const router = Router();
-    app.use(SPIPath, router);
-    router.get("/download", (req, res) => {
-        res.send(metadata);
-    });
+  const router = Router();
+  app.use(SPIPath, router);
 
-    const spiRoot = path.join(__dirname, 'spi');
-    fs.exists(spiRoot, exists => {
-        if (exists) {
-            loadSPIModules(spiRoot, app, router);
-        }
-    });
+  const spiRoot = path.join(__dirname, "spi");
+  fs.exists(spiRoot, (exists) => {
+    if (exists) {
+      loadSPIModules(spiRoot, app, router);
+    }
+  });
 }
 
 function loadSPIModules(fullSPIPath: string, app: Express, router: Router) {
-    fs.lstat(fullSPIPath, function (err, stat) {
-        if (stat.isDirectory()) {
-            fs.readdir(fullSPIPath, function (dirErr, files) {
-                files.filter(fileName => fileName.endsWith(".js"))
-                    .map(fileName => path.join(fullSPIPath, fileName))
-                    .forEach(filePath => loadSPIModules(filePath, app, router));
-            });
-        } else {
-            const module = require(fullSPIPath);
-            if (typeof module === "function") {
-                module(app, metadata, router);
-            }
-        }
-    });
+  fs.lstat(fullSPIPath, function (err, stat) {
+    if (stat.isDirectory()) {
+      fs.readdir(fullSPIPath, function (dirErr, files) {
+        files
+          .filter((fileName) => fileName.endsWith(".js"))
+          .map((fileName) => path.join(fullSPIPath, fileName))
+          .forEach((filePath) => loadSPIModules(filePath, app, router));
+      });
+    } else {
+      const module = require(fullSPIPath);
+      if (typeof module === "function") {
+        module(app, router);
+      }
+    }
+  });
 }
